7031725fc2f605c8009f5fa593de14de8072a734
modyhoon <taehkim@student.42seoul.kr>
2021-11-01 16:17:15 +0900

    feat: 스토리보드 제거 및 HomeViewController 생성

--- BBus/BBus/HomeViewController.swift
+++ BBus/BBus/HomeViewController.swift
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        self.view.backgroundColor = UIColor.systemBackground
     }
-
-
 }
 
