c9d4f6de8a47e91160fd6a5abfdfb430b209cf0c
S057_최수정 <babeeboo2000@gmail.com>
2021-11-02 15:12:22 +0900

    feat: #6 BusRouteViewController 기본 UI 구조 생성

--- BBus/BBus/BusRoute/BusRouteViewController.swift
+++ BBus/BBus/BusRoute/BusRouteViewController.swift
@@ -9,21 +9,61 @@ import UIKit
 
 class BusRouteViewController: UIViewController {
 
+    private lazy var busRouteView = BusRouteView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.configureLayout()
+        self.configureStatusBarBackgroundColor(to: UIColor.systemBlue)
+        self.configureNavigationBarBackgroundColor(to: UIColor.systemBlue)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        self.configureStatusBarBackgroundColor(to: UIColor.white)
+        self.configureNavigationBarBackgroundColor(to: UIColor.white)
     }
-    */
+    
+    private func configureNavigationBarBackgroundColor(to color: UIColor) {
+        self.navigationController?.navigationBar.backgroundColor = color
+    }
+    
+    private func configureStatusBarBackgroundColor(to color: UIColor) {
+        if #available(iOS 13.0, *) {
+            let app = UIApplication.shared
+            let statusBarHeight: CGFloat = app.statusBarFrame.size.height
+            
+            let statusbarView = UIView()
+            statusbarView.backgroundColor = color
+            self.view.addSubview(statusbarView)
 
+            statusbarView.translatesAutoresizingMaskIntoConstraints = false
+            statusbarView.heightAnchor
+                .constraint(equalToConstant: statusBarHeight).isActive = true
+            statusbarView.widthAnchor
+                .constraint(equalTo: self.view.widthAnchor, multiplier: 1.0).isActive = true
+            statusbarView.topAnchor
+                .constraint(equalTo: self.view.topAnchor).isActive = true
+            statusbarView.centerXAnchor
+                .constraint(equalTo: self.view.centerXAnchor).isActive = true
+        } else {
+            let statusBar = UIApplication.shared.value(forKeyPath: "statusBarWindow.statusBar") as? UIView
+            statusBar?.backgroundColor = color
+        }
+    }
+
+    private func configureLayout() {
+        self.view.backgroundColor = UIColor.systemBackground
+        
+        self.busRouteView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(self.busRouteView)
+        NSLayoutConstraint.activate([
+            self.busRouteView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            self.busRouteView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            self.busRouteView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            self.busRouteView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor)
+        ])
+    }
 }
--- BBus/BBus/BusRoute/View/BusRouteView.swift
+++ BBus/BBus/BusRoute/View/BusRouteView.swift
@@ -7,14 +7,88 @@
 
 import UIKit
 
-class BusRouteView: UIStackView {
+class BusRouteView: UIView {
+    
+    private lazy var colorBackgroundView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.systemBlue
+        return view
+    }()
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    private lazy var busRouteScrollView: UIScrollView = UIScrollView()
+    
+    private lazy var busRouteScrollContentsView: UIView = UIView()
+    
+    private lazy var busHeaderView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.systemBlue
+        return view
+    }()
+    
+    private lazy var busRouteTableView: UITableView = {
+        let tableView = UITableView()
+//        tableView.register(<#T##nib: UINib?##UINib?#>, forCellReuseIdentifier: <#T##String#>)
+        tableView.backgroundColor = UIColor.cyan
+        return tableView
+    }()
+
+    convenience init() {
+        self.init(frame: CGRect())
+        self.backgroundColor = UIColor.systemBackground
+        self.configureLayout()
     }
-    */
 
+    func configureLayout() {
+        self.addSubview(self.colorBackgroundView)
+        self.colorBackgroundView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.colorBackgroundView.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 0.5),
+            self.colorBackgroundView.topAnchor.constraint(equalTo: self.topAnchor),
+            self.colorBackgroundView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            self.colorBackgroundView.trailingAnchor.constraint(equalTo: self.trailingAnchor)
+        ])
+        
+        self.addSubview(self.busRouteScrollView)
+        self.busRouteScrollView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.busRouteScrollView.topAnchor.constraint(equalTo: self.topAnchor),
+            self.busRouteScrollView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            self.busRouteScrollView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            self.busRouteScrollView.trailingAnchor.constraint(equalTo: self.trailingAnchor)
+        ])
+
+        self.busRouteScrollContentsView.addSubview(self.busHeaderView)
+        self.busHeaderView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.busHeaderView.heightAnchor.constraint(equalToConstant: 250),
+            self.busHeaderView.leadingAnchor.constraint(equalTo: self.busRouteScrollContentsView.leadingAnchor),
+            self.busHeaderView.trailingAnchor.constraint(equalTo: self.busRouteScrollContentsView.trailingAnchor),
+            self.busHeaderView.topAnchor.constraint(equalTo: self.busRouteScrollContentsView.topAnchor)
+        ])
+        
+        self.busRouteScrollContentsView.addSubview(self.busRouteTableView)
+        self.busRouteTableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.busRouteTableView.heightAnchor.constraint(equalToConstant: 1000),
+            self.busRouteTableView.leadingAnchor.constraint(equalTo: self.busRouteScrollContentsView.leadingAnchor),
+            self.busRouteTableView.trailingAnchor.constraint(equalTo: self.busRouteScrollContentsView.trailingAnchor),
+            self.busRouteTableView.topAnchor.constraint(equalTo: self.busHeaderView.bottomAnchor),
+            self.busRouteTableView.bottomAnchor.constraint(equalTo: self.busRouteScrollContentsView.bottomAnchor)
+        ])
+        
+        self.busRouteScrollView.addSubview(self.busRouteScrollContentsView)
+        self.busRouteScrollContentsView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.busRouteScrollContentsView.topAnchor.constraint(equalTo: self.busRouteScrollView.contentLayoutGuide.topAnchor),
+            self.busRouteScrollContentsView.leadingAnchor.constraint(equalTo: self.busRouteScrollView.contentLayoutGuide.leadingAnchor),
+            self.busRouteScrollContentsView.trailingAnchor.constraint(equalTo: self.busRouteScrollView.contentLayoutGuide.trailingAnchor),
+            self.busRouteScrollContentsView.bottomAnchor.constraint(equalTo: self.busRouteScrollView.contentLayoutGuide.bottomAnchor),
+            self.busRouteScrollContentsView.widthAnchor.constraint(equalTo: self.busRouteScrollView.frameLayoutGuide.widthAnchor)
+        ])
+    }
+    
+    func configureBusColor(to color: UIColor) {
+        self.colorBackgroundView.backgroundColor = color
+        self.busHeaderView.backgroundColor = color
+    }
 }
