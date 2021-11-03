cdf42d040ac48a6b4fba676f385fb7b0acd3ad1f
S057_최수정 <babeeboo2000@gmail.com>
2021-11-03 18:39:22 +0900

    refactor: #6 StatusBar 색상 설정 제거
    1. StatusBar 위치에 뷰를 생성하여 색상 설정해주는 방식에서 view.backgroundColor 설정하는 방식으로 변경함.

--- BBus/BBus/BusRoute/BusRouteViewController.swift
+++ BBus/BBus/BusRoute/BusRouteViewController.swift
@@ -39,9 +39,9 @@ class BusRouteViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.configureBackgroundColor()
         self.configureLayout()
         self.configureDelegate()
-        self.configureStatusBarBackgroundColor(to: UIColor.systemBlue)
         self.configureTableView()
         self.busRouteView.addBusTag()
         
@@ -61,34 +61,8 @@ class BusRouteViewController: UIViewController {
         self.busRouteView.busRouteTableView.delegate = self
         self.busRouteView.busRouteTableView.dataSource = self
     }
-    
-    private func configureStatusBarBackgroundColor(to color: UIColor) {
-        if #available(iOS 13.0, *) {
-            let app = UIApplication.shared
-            let statusBarHeight: CGFloat = app.statusBarFrame.size.height
-            
-            let statusbarView = UIView()
-            statusbarView.backgroundColor = color
-            self.view.addSubview(statusbarView)
-
-            statusbarView.translatesAutoresizingMaskIntoConstraints = false
-            statusbarView.heightAnchor
-                .constraint(equalToConstant: statusBarHeight).isActive = true
-            statusbarView.widthAnchor
-                .constraint(equalTo: self.view.widthAnchor, multiplier: 1.0).isActive = true
-            statusbarView.topAnchor
-                .constraint(equalTo: self.view.topAnchor).isActive = true
-            statusbarView.centerXAnchor
-                .constraint(equalTo: self.view.centerXAnchor).isActive = true
-        } else {
-            let statusBar = UIApplication.shared.value(forKeyPath: "statusBarWindow.statusBar") as? UIView
-            statusBar?.backgroundColor = color
-        }
-    }
 
     private func configureLayout() {
-        self.view.backgroundColor = UIColor.systemBackground
-        
         self.busRouteView.translatesAutoresizingMaskIntoConstraints = false
         self.view.addSubview(self.busRouteView)
         NSLayoutConstraint.activate([
@@ -101,6 +75,10 @@ class BusRouteViewController: UIViewController {
 
     private func configureDelegate() {
         self.busRouteView.configureDelegate(self)
+    }
+    
+    private func configureBackgroundColor() {
+        self.view.backgroundColor = Color.blueBus
     }
 }
 
