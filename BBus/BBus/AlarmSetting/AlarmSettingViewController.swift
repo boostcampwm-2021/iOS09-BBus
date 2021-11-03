6ea2f529c99749a6739ff0d133e6d9be64a939b4
FreeDeveloper97 <freedeveloper97@gmail.com>
2021-11-03 23:46:16 +0900

    feat: #8 AlarmSettingViewController 기본 UI 구성

--- BBus/BBus/AlarmSetting/AlarmSettingViewController.swift
+++ BBus/BBus/AlarmSetting/AlarmSettingViewController.swift
@@ -9,21 +9,94 @@ import UIKit
 
 class AlarmSettingViewController: UIViewController {
 
+    enum Color {
+        static let white = UIColor.white
+        static let black = UIColor.black
+        static let clear = UIColor.clear
+        static let lightGray = UIColor.lightGray
+        static let darkGray = UIColor.darkGray
+        static let blueBus = UIColor.systemBlue
+        static let tableViewSeperator = UIColor.systemGray6
+        static let tableViewCellSubTitle = UIColor.systemGray
+        static let tagBusNumber = UIColor.darkGray
+        static let tagBusCongestion = UIColor.red
+        static let greenLine = UIColor.green
+        static let redLine = UIColor.red
+        static let tableBackground = UIColor.systemGray5
+    }
+
+    private lazy var alarmSettingView = AlarmSettingView()
+    private lazy var customNavigationBar = CustomNavigationBar()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.view.backgroundColor = Color.white
+        self.navigationController?.isNavigationBarHidden = true
+        self.configureLayout()
+        self.configureDelegate()
+        self.configureTableView()
+        self.configureColor()
+        self.configureMOCKDATA()
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func configureTableView() {
+        self.alarmSettingView.alarmTableView.delegate = self
+        self.alarmSettingView.alarmTableView.dataSource = self
     }
-    */
 
+    private func configureLayout() {
+        self.view.addSubview(self.customNavigationBar)
+        self.customNavigationBar.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.customNavigationBar.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            self.customNavigationBar.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            self.customNavigationBar.trailingAnchor.constraint(equalTo: self.view.trailingAnchor)
+        ])
+
+        self.view.addSubview(self.alarmSettingView)
+        self.alarmSettingView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.alarmSettingView.topAnchor.constraint(equalTo: self.customNavigationBar.bottomAnchor),
+            self.alarmSettingView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            self.alarmSettingView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            self.alarmSettingView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor)
+        ])
+    }
+
+    private func configureDelegate() {
+        self.alarmSettingView.configureDelegate(self)
+        self.customNavigationBar.configureDelegate(self)
+    }
+
+    private func configureColor() {
+        self.customNavigationBar.configureTintColor(color: Color.black)
+        self.customNavigationBar.configureAlpha(alpha: 1)
+    }
+
+    private func configureMOCKDATA() {
+        self.customNavigationBar.configureTitle(NSAttributedString(string: "461 ∙ 예술인마을.사당초등학교"))
+    }
+}
+
+extension AlarmSettingViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+extension AlarmSettingViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+}
+
+extension AlarmSettingViewController: BackButtonDelegate {
+    func touchedBackButton() {
+        self.navigationController?.popViewController(animated: true)
+    }
 }
--- BBus/BBus/AlarmSetting/View/AlarmSettingView.swift
+++ BBus/BBus/AlarmSetting/View/AlarmSettingView.swift
@@ -7,14 +7,76 @@
 
 import UIKit
 
-class AlarmSettingView: UIStackView {
+class AlarmSettingView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    private lazy var alarmScrollView: UIScrollView = UIScrollView()
+
+    private lazy var alarmScrollViewContentsView: UIView = UIView()
+
+    lazy var alarmTableView: UITableView = {
+        let tableView = UITableView()
+//        tableView.register(<#T##nib: UINib?##UINib?#>, forCellReuseIdentifier: <#T##String#>)
+        tableView.separatorInset = UIEdgeInsets(top: 0, left: 90, bottom: 0, right: 0)
+        tableView.separatorColor = AlarmSettingViewController.Color.tableViewSeperator
+        tableView.backgroundColor = AlarmSettingViewController.Color.tableBackground
+        return tableView
+    }()
+
+    lazy var reportWrongDataButton: UIButton = {
+        let button = UIButton()
+        button.titleLabel?.textAlignment = .center
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 15)
+        button.setTitle("잘못된 정보 신고", for: .normal)
+        button.setTitleColor(AlarmSettingViewController.Color.lightGray, for: .normal)
+        return button
+    }()
+
+    convenience init() {
+        self.init(frame: CGRect())
+
+        self.backgroundColor = AlarmSettingViewController.Color.tableBackground
+        self.configureLayout()
     }
-    */
 
+    func configureLayout() {
+        self.addSubview(self.alarmScrollView)
+        self.alarmScrollView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.alarmScrollView.topAnchor.constraint(equalTo: self.topAnchor),
+            self.alarmScrollView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            self.alarmScrollView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            self.alarmScrollView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+
+        self.alarmScrollViewContentsView.addSubview(self.alarmTableView)
+        self.alarmTableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.alarmTableView.heightAnchor.constraint(equalToConstant: 800),
+            self.alarmTableView.leadingAnchor.constraint(equalTo: self.alarmScrollViewContentsView.leadingAnchor),
+            self.alarmTableView.trailingAnchor.constraint(equalTo: self.alarmScrollViewContentsView.trailingAnchor),
+            self.alarmTableView.topAnchor.constraint(equalTo: self.alarmScrollViewContentsView.topAnchor, constant: 20),
+            self.alarmTableView.bottomAnchor.constraint(equalTo: self.alarmScrollViewContentsView.bottomAnchor, constant: -100)
+        ])
+
+        self.alarmScrollViewContentsView.addSubview(self.reportWrongDataButton)
+        self.reportWrongDataButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.reportWrongDataButton.centerXAnchor.constraint(equalTo: self.alarmScrollViewContentsView.centerXAnchor),
+            self.reportWrongDataButton.bottomAnchor.constraint(equalTo: self.alarmScrollViewContentsView.bottomAnchor, constant: -16)
+        ])
+
+        self.alarmScrollView.addSubview(self.alarmScrollViewContentsView)
+        self.alarmScrollViewContentsView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.alarmScrollViewContentsView.topAnchor.constraint(equalTo: self.alarmScrollView.contentLayoutGuide.topAnchor),
+            self.alarmScrollViewContentsView.leadingAnchor.constraint(equalTo: self.alarmScrollView.contentLayoutGuide.leadingAnchor),
+            self.alarmScrollViewContentsView.trailingAnchor.constraint(equalTo: self.alarmScrollView.contentLayoutGuide.trailingAnchor),
+            self.alarmScrollViewContentsView.bottomAnchor.constraint(equalTo: self.alarmScrollView.contentLayoutGuide.bottomAnchor),
+            self.alarmScrollViewContentsView.widthAnchor.constraint(equalTo: self.alarmScrollView.frameLayoutGuide.widthAnchor)
+        ])
+    }
+
+    func configureDelegate(_ delegate: BackButtonDelegate) {
+//        self.customNavigationBar.configureDelegate(delegate)
+    }
 }
