7b4c36c8b0fc59b1589690f0a5852d9bc16f654f
modyhoon <taehkim@student.42seoul.kr>
2021-11-08 12:56:53 +0900

    refactor: #20 SearchResultScrollView 메서드 파라미터 개행

--- BBus/BBus/Search/View/SearchResultScrollView.swift
+++ BBus/BBus/Search/View/SearchResultScrollView.swift
@@ -15,14 +15,18 @@ class SearchResultScrollView: UIScrollView {
         let collectionView = UICollectionView(frame: CGRect(), collectionViewLayout: self.collectionViewLayout())
         collectionView.backgroundColor = MyColor.bbusLightGray
         collectionView.register(SearchResultCollectionViewCell.self, forCellWithReuseIdentifier: SearchResultCollectionViewCell.identifier)
-        collectionView.register(SimpleCollectionHeaderView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: SimpleCollectionHeaderView.identifier)
+        collectionView.register(SimpleCollectionHeaderView.self,
+                                forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+                                withReuseIdentifier: SimpleCollectionHeaderView.identifier)
         return collectionView
     }()
     private lazy var stationResultCollectionView: UICollectionView = {
         let collectionView = UICollectionView(frame: CGRect(), collectionViewLayout: self.collectionViewLayout())
         collectionView.backgroundColor = MyColor.bbusLightGray
         collectionView.register(SearchResultCollectionViewCell.self, forCellWithReuseIdentifier: SearchResultCollectionViewCell.identifier)
-        collectionView.register(SimpleCollectionHeaderView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: SimpleCollectionHeaderView.identifier)
+        collectionView.register(SimpleCollectionHeaderView.self,
+                                forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+                                withReuseIdentifier: SimpleCollectionHeaderView.identifier)
         return collectionView
     }()
     private lazy var leftFakeIndicatorView: UIView = {
