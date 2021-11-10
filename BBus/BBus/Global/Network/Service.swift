1abd56340d7941a664fdbec7de14c5b53b3af47c
JISU LEE <wltn102998@gmail.com>
2021-11-10 16:21:23 +0900

    feat: #23 Service 초기 구현 완료

--- BBus/BBus/Global/Network/Service.swift
+++ BBus/BBus/Global/Network/Service.swift
@@ -6,8 +6,9 @@
 //
 
 import Foundation
+import Combine
 
-enum AccessKey {
+enum APIType {
     case busArrive, routeInfo, stationInfo, busLocation
     
     func toString() -> String? {
@@ -24,7 +25,33 @@ enum AccessKey {
     }
 }
 
+enum NetworkError: Error {
+    case accessKeyError, urlError
+}
+
+
+// TODO: - Service Return Type 수정 필요
 class Service {
     static let shared = Service()
+    
     private init() { }
+    
+    func get(url: String, api: APIType, params: [String: String]) -> Result<URLSession.DataTaskPublisher, NetworkError> {
+        guard let accessKey = api.toString() else { return .failure(NetworkError.accessKeyError) }
+        guard var components = URLComponents(string: url + "?serviceKey" + accessKey) else { return .failure(NetworkError.urlError) }
+        
+        var items: [URLQueryItem] = []
+        params.forEach() { item in
+            items.append(URLQueryItem(name: item.key, value: item.value))
+        }
+        components.queryItems = items
+        
+        if let url = components.url {
+            var request = URLRequest(url: url)
+            request.httpMethod = "GET"
+            return .success(URLSession.init(configuration: .default).dataTaskPublisher(for: request))
+        } else {
+            return .failure(NetworkError.urlError)
+        }
+    }
 }
