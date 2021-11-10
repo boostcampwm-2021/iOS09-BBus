bdb133e8440e2770712868ddc05984138a405f59
S057_최수정 <babeeboo2000@gmail.com>
2021-11-10 17:04:00 +0900

    add: #25 BusRouteDTO 추가

---
+++ BBus/BBus/Global/DTO/BusRouteDTO.swift
@@ -0,0 +1,25 @@
+//
+//  BusRouteDTO.swift
+//  BBus
+//
+//  Created by 최수정 on 2021/11/10.
+//
+
+import Foundation
+
+struct BusRouteDTO: Codable {
+    let routeID: Int
+    let busRouteName: String
+    let routeType: RouteType
+    let startStation: String
+    let endStation: String
+}
+
+enum RouteType: String, Codable {
+    case mainLine = "간선"
+    case broadArea = "광역"
+    case customized = "맞춤"
+    case circulation = "순환"
+    case lateNight = "심야"
+    case localLine = "지선"
+}
