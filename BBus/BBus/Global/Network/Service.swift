fc1c9ea699267d6c1ea6f1a804242f5da89f7074
JISU LEE <wltn102998@gmail.com>
2021-11-10 15:47:27 +0900

    feat: #23 환경변수 설정 및 AccessKey enum 선언 완료

--- BBus/BBus/Global/Network/Service.swift
+++ BBus/BBus/Global/Network/Service.swift
@@ -7,6 +7,23 @@
 
 import Foundation
 
+enum AccessKey {
+    case busArrive, routeInfo, stationInfo, busLocation
+    
+    func toString() -> String? {
+        switch self {
+        case .busArrive :
+            return Bundle.main.infoDictionary?["BUS_ARRIVE_API_ACCESS_KEY"] as? String
+        case .busLocation :
+            return Bundle.main.infoDictionary?["BUS_ARRIVE_API_ACCESS_KEY"] as? String
+        case .routeInfo :
+            return Bundle.main.infoDictionary?["BUS_ARRIVE_API_ACCESS_KEY"] as? String
+        case .stationInfo :
+            return Bundle.main.infoDictionary?["BUS_ARRIVE_API_ACCESS_KEY"] as? String
+        }
+    }
+}
+
 class Service {
     static let shared = Service()
     private init() { }
--- BBus/BBus/Home/HomeViewController.swift
+++ BBus/BBus/Home/HomeViewController.swift
@@ -27,7 +27,6 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Home"
-
         self.configureColor()
         self.configureLayout()
         self.homeView.configureLayout()
