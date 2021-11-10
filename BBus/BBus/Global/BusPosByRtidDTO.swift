c574b83da3176dad8ff562c6f88fe4c8956fbcdf
S057_최수정 <babeeboo2000@gmail.com>
2021-11-11 01:55:47 +0900

    feat: #29 BusPosByRtIdDTO 구현

--- BBus/BBus/Global/BusPosByRtidDTO.swift
+++ BBus/BBus/Global/BusPosByRtidDTO.swift
@@ -7,24 +7,24 @@
 
 import Foundation
 
-//struct BusPosByRtidDTO: BBusXMLDTO {
-//    let busType: Int
-//    let congestion: Int
-//    let plainNumber: String
-//    let sectionOrder: Int
-//    
-//    init?(dict: [String : [Any]]) {
-//        guard let busTypeString = dict["busType"],
-//              let busType = Int(busTypeString),
-//              let congestionString = dict["congetion"],
-//              let congestion = Int(congestionString),
-//              let plainNumber = dict["plainNo"],
-//              let sectOrd = dict["sectOrd"],
-//              let sectionOrder = Int(sectOrd) else { return nil }
-//        
-//        self.busType = busType
-//        self.congestion = congestion
-//        self.plainNumber = plainNumber
-//        self.sectionOrder = sectionOrder
-//    }
-//}
+struct BusPosByRtidDTO: BBusXMLDTO {
+    let busType: Int
+    let congestion: Int
+    let plainNumber: String
+    let sectionOrder: Int
+    
+    init?(dict: [String : [Any]]) {
+        guard let busTypeString = ((dict["busType"]?[0] as? [String:[Any]])?["bbus"] as? [String])?.reduce("", { $0 + $1 }),
+              let busType = Int(busTypeString),
+              let congestionString = ((dict["congetion"]?[0] as? [String:[Any]])?["bbus"] as? [String])?.reduce("", { $0 + $1 }),
+              let congestion = Int(congestionString),
+              let plainNumber = ((dict["plainNo"]?[0] as? [String:[Any]])?["bbus"] as? [String])?.reduce("", { $0 + $1 }),
+              let sectOrd = ((dict["sectOrd"]?[0] as? [String:[Any]])?["bbus"] as? [String])?.reduce("", { $0 + $1 }),
+              let sectionOrder = Int(sectOrd) else { return nil }
+        
+        self.busType = busType
+        self.congestion = congestion
+        self.plainNumber = plainNumber
+        self.sectionOrder = sectionOrder
+    }
+}
