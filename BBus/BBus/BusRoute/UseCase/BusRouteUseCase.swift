a6897f3feb6b509a07d8b5c84c581f602d320b51
FreeDeveloper97 <freedeveloper97@gmail.com>
2021-11-11 17:55:17 +0900

    feat: #52 BusRouteUsecase 기본틀 작성

--- BBus/BBus/BusRoute/UseCase/BusRouteUseCase.swift
+++ BBus/BBus/BusRoute/UseCase/BusRouteUseCase.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import Combine
+
+class BusRouteUsecase {
+
+    private let usecases: GetRouteListUsecase
+    @Published var header: BusRouteDTO?
+    private var cancellable: AnyCancellable?
+    static let thread = DispatchQueue(label: "BusRoute")
+
+    init(usecases: GetRouteListUsecase, busRouteId: String) {
+        self.usecases = usecases
+        self.searchHeader(busRouteId: busRouteId)
+    }
+
+    private func searchHeader(busRouteId: String) {
+        self.cancellable = usecases.getRouteList()
+            .receive(on: Self.thread)
+            .decode(type: [BusRouteDTO].self, decoder: JSONDecoder())
+            .sink(receiveCompletion: { error in
+                if case .failure(let error) = error {
+                    print(error)
+                }
+            }, receiveValue: { routeList in
+                self.header = routeList[0]
+            })
+    }
+}
