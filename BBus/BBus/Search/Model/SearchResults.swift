6374d83d98f50c1e3861b47962fc5eb32325ca7a
S057_최수정 <babeeboo2000@gmail.com>
2021-11-14 17:33:56 +0900

    add: #69 전체 검색 결과에 대응되는 SearchResults 타입 추가

---
+++ BBus/BBus/Search/Model/SearchResults.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResult.swift
+//  BBus
+//
+//  Created by 최수정 on 2021/11/14.
+//
+
+import Foundation
+
+struct SearchResults {
+    var busSearchResults: [BusSearchResult]
+    var stationSearchResults: [StationSearchResult]
+}
