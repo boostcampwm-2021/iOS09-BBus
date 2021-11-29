f182f5975563d74e9aeb9c318fd523b40c1f0e7c
modyhoon <taehkim@student.42seoul.kr>
2021-11-29 23:20:26 +0900

    refactor: #309 custom refresh button 생성

---
+++ BBus/BBus/Global/CustomView/RefreshButton.swift
@@ -0,0 +1,26 @@
+//
+//  RefreshButton.swift
+//  BBus
+//
+//  Created by 김태훈 on 2021/11/29.
+//
+
+import UIKit
+
+final class RefreshButton: ThrottleButton {
+
+    private let refreshButtonWidth: CGFloat = 50
+
+    convenience init() {
+        self.init(frame: CGRect())
+        self.configureUI()
+    }
+
+    private func configureUI() {
+        self.setImage(BBusImage.refresh, for: .normal)
+        self.layer.cornerRadius = self.refreshButtonWidth / 2
+        self.tintColor = BBusColor.white
+        self.backgroundColor = BBusColor.darkGray
+    }
+
+}
---
+++ BBus/BBus/Global/CustomView/RefreshableView.swift
@@ -0,0 +1,20 @@
+//
+//  RefreshableView.swift
+//  BBus
+//
+//  Created by 김태훈 on 2021/11/29.
+//
+
+import UIKit
+
+class RefreshableView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
