40f833dba7673922e5729b643b848bfe6aa9553a
S057_최수정 <babeeboo2000@gmail.com>
2021-12-01 13:14:38 +0900

    test: Mode, Error, cancellables 선언

--- BBus/AlarmSettingViewModelTests/AlarmSettingViewModelTests.swift
+++ BBus/AlarmSettingViewModelTests/AlarmSettingViewModelTests.swift
@@ -6,27 +6,26 @@
 //
 
 import XCTest
+import Combine
 
 class AlarmSettingViewModelTests: XCTestCase {
+    enum MOCKMode {
+        case success
+    }
+    
+    enum TestError: Error {
+        
+    }
+    
+    private var cancellables: Set<AnyCancellable>!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        self.cancellables = []
+        super.setUp()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        self.cancellables = nil
+        super.tearDown()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
